1cd6716f142d61e823668e9ee55f3f403ed962a0
graldij <graldij@student.ethz.ch>
2023-05-30 00:14:18 +0200

    [MOD] NOT FINISHED -- Cleanup and adaptatino of interfuser-carla interface for our purpose.

--- leaderboard/team_code/DiffuseDrive_agent.py
+++ leaderboard/team_code/DiffuseDrive_agent.py
@@ -20,8 +20,6 @@ from timm.models import create_model
 from team_code.utils import lidar_to_histogram_features, transform_2d_points
 from team_code.planner import RoutePlanner
 from leaderboard.team_code.DiffuseDrive_controller import DiffuseDriveController
-from team_code.render import render, render_self_car, render_waypoints
-from team_code.tracker import Tracker
 
 import math
 import yaml
@@ -51,6 +49,7 @@ class DisplayInterface(object):
         )
         pygame.display.set_caption("Human Agent")
 
+    # TODO Marcus: have no clue of what this does
     def run_interface(self, input_data):
         rgb = input_data['rgb']
         rgb_left = input_data['rgb_left']
@@ -165,12 +164,15 @@ class DiffuseDriveAgent(autonomous_agent.AutonomousAgent):
         self.step = -1
         self.wall_start = time.time()
         self.initialized = False
+        
+        # TODO Marcus in theory it should not matter much what size we input into the evaluation network as it is resized anyway
         self.rgb_front_transform = create_carla_rgb_transform(224)
+        
         self.rgb_left_transform = create_carla_rgb_transform(128)
         self.rgb_right_transform = create_carla_rgb_transform(128)
         self.rgb_center_transform = create_carla_rgb_transform(128, need_scale=False)
 
-        self.tracker = Tracker()
+        # self.tracker = Tracker()
 
         self.input_buffer = {
             "rgb": deque(),
@@ -185,36 +187,24 @@ class DiffuseDriveAgent(autonomous_agent.AutonomousAgent):
         self.config = imp.load_source("MainModel", path_to_conf_file).GlobalConfig()
         self.skip_frames = self.config.skip_frames
         self.controller = DiffuseDriveController(self.config)
-        if isinstance(self.config.model, list):
-            self.ensemble = True
-        else:
-            self.ensemble = False
 
-        if self.ensemble:
-            for i in range(len(self.config.model)):
-                self.nets = []
-                net = create_model(self.config.model[i])
-                path_to_model_file = self.config.model_path[i]
-                print('load model: %s' % path_to_model_file)
-                net.load_state_dict(torch.load(path_to_model_file)["state_dict"])
-                net.cuda()
-                net.eval()
-                self.nets.append(net)
-        else:
-            self.net = create_model(self.config.model)
-            path_to_model_file = self.config.model_path
-            print('load model: %s' % path_to_model_file)
-            self.net.load_state_dict(torch.load(path_to_model_file)["state_dict"])
-            self.net.cuda()
-            self.net.eval()
+        # TODO Jacopo: load the model
+        self.net = create_model(self.config.model)
+        path_to_model_file = self.config.model_path
+        print('load model: %s' % path_to_model_file)
+        self.net.load_state_dict(torch.load(path_to_model_file)["state_dict"])
+        self.net.cuda()
+        self.net.eval()
+        
         self.softmax = torch.nn.Softmax(dim=1)
-        self.traffic_meta_moving_avg = np.zeros((400, 7))
         self.momentum = self.config.momentum
         self.prev_lidar = None
         self.prev_control = None
         self.prev_surround_map = None
 
         self.save_path = None
+        
+        
         if SAVE_PATH is not None:
             now = datetime.datetime.now()
             string = pathlib.Path(os.environ["ROUTES"]).stem + "_"
@@ -237,6 +227,7 @@ class DiffuseDriveAgent(autonomous_agent.AutonomousAgent):
         self.initialized = True
 
     def _get_position(self, tick_data):
+        # TODO Marcus: probably here need to adapt to our coordinate system (ego)
         gps = tick_data["gps"]
         gps = (gps - self._route_planner.mean) * self._route_planner.scale
         return gps
@@ -363,6 +354,8 @@ class DiffuseDriveAgent(autonomous_agent.AutonomousAgent):
         result["lidar"] = self.prev_lidar
 
         result["gps"] = pos
+        
+        # TODO Marcus: here we plan the route and take next waypoint and action 
         next_wp, next_cmd = self._route_planner.run_step(pos)
         result["next_command"] = next_cmd.value
         result['measurements'] = [pos[0], pos[1], compass, speed]
@@ -370,6 +363,7 @@ class DiffuseDriveAgent(autonomous_agent.AutonomousAgent):
         theta = compass + np.pi / 2
         R = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
 
+        # TODO Marcus: not sure exactly why this is needed
         local_command_point = np.array([next_wp[0] - pos[0], next_wp[1] - pos[1]])
         local_command_point = R.T.dot(local_command_point)
         result["target_point"] = local_command_point
@@ -415,6 +409,8 @@ class DiffuseDriveAgent(autonomous_agent.AutonomousAgent):
             .float()
         )
 
+        # TODO Marcus: what exactly are the categories of the one hot encoding?
+        # TODO Marcus: might be interesting to feed also the velocity as input of our diffusion model?
         cmd_one_hot = [0, 0, 0, 0, 0, 0]
         cmd = command - 1
         cmd_one_hot[cmd] = 1
@@ -434,57 +430,21 @@ class DiffuseDriveAgent(autonomous_agent.AutonomousAgent):
         input_data["lidar"] = (
             torch.from_numpy(tick_data["lidar"]).float().cuda().unsqueeze(0)
         )
-        if self.ensemble:
-            outputs = []
-            with torch.no_grad():
-                for net in self.nets:
-                    output = net(input_data)
-                    outputs.append(output)
-            traffic_meta = torch.mean(torch.stack([x[0] for x in outputs]), 0)
-            pred_waypoints = torch.mean(torch.stack([x[1] for x in outputs]), 0)
-            is_junction = torch.mean(torch.stack([x[2] for x in outputs]), 0)
-            traffic_light_state = torch.mean(torch.stack([x[3] for x in outputs]), 0)
-            stop_sign = torch.mean(torch.stack([x[4] for x in outputs]), 0)
-            bev_feature = torch.mean(torch.stack([x[5] for x in outputs]), 0)
-        else:
-            with torch.no_grad():
-                (
-                    traffic_meta,
-                    pred_waypoints,
-                    is_junction,
-                    traffic_light_state,
-                    stop_sign,
-                    bev_feature,
-                ) = self.net(input_data)
-        traffic_meta = traffic_meta.detach().cpu().numpy()[0]
-        bev_feature = bev_feature.detach().cpu().numpy()[0]
+        
+        
+        
+        # TODO Jacopo: adapt to our model
+        with torch.no_grad():
+            pred_waypoints = self.net(input_data)
+
         pred_waypoints = pred_waypoints.detach().cpu().numpy()[0]
-        is_junction = self.softmax(is_junction).detach().cpu().numpy().reshape(-1)[0]
-        traffic_light_state = (
-            self.softmax(traffic_light_state).detach().cpu().numpy().reshape(-1)[0]
-        )
-        stop_sign = self.softmax(stop_sign).detach().cpu().numpy().reshape(-1)[0]
 
 
-        if self.step % 2 == 0 or self.step < 4:
-            traffic_meta = self.tracker.update_and_predict(traffic_meta.reshape(20, 20, -1), tick_data['gps'], tick_data['compass'], self.step // 2)
-            traffic_meta = traffic_meta.reshape(400, -1)
-            self.traffic_meta_moving_avg = (
-                self.momentum * self.traffic_meta_moving_avg
-                + (1 - self.momentum) * traffic_meta
-            )
-        traffic_meta = self.traffic_meta_moving_avg
-
-        tick_data["raw"] = traffic_meta
-        tick_data["bev_feature"] = bev_feature
-
+        # call the controller to compute the control out of the generated
+        # TODO Jacopo: adapt to our outputs
         steer, throttle, brake, meta_infos = self.controller.run_step(
             velocity,
             pred_waypoints,
-            is_junction,
-            traffic_light_state,
-            stop_sign,
-            self.traffic_meta_moving_avg,
         )
 
         if brake < 0.05:
@@ -492,66 +452,24 @@ class DiffuseDriveAgent(autonomous_agent.AutonomousAgent):
         if brake > 0.1:
             throttle = 0.0
 
+        # TODO Marcus: here you probably need to check if this works out of the box
         control = carla.VehicleControl()
         control.steer = float(steer)
         control.throttle = float(throttle)
         control.brake = float(brake)
 
-        surround_map, box_info = render(traffic_meta.reshape(20, 20, 7), pixels_per_meter=20)
-        surround_map = surround_map[:400, 160:560]
-        surround_map = np.stack([surround_map, surround_map, surround_map], 2)
-
-        self_car_map = render_self_car(
-            loc=np.array([0, 0]),
-            ori=np.array([0, -1]),
-            box=np.array([2.45, 1.0]),
-            color=[1, 1, 0], pixels_per_meter=20
-        )[:400, 160:560]
-
         pred_waypoints = pred_waypoints.reshape(-1, 2)
-        safe_index = 10
-        for i in range(10):
-            if pred_waypoints[i, 0] ** 2 + pred_waypoints[i, 1] ** 2> (meta_infos[3]+0.5) ** 2:
-                safe_index = i
-                break
-        wp1 = render_waypoints(pred_waypoints[:safe_index], pixels_per_meter=20, color=(0, 255, 0))[:400, 160:560]
-        wp2 = render_waypoints(pred_waypoints[safe_index:], pixels_per_meter=20, color=(255, 0, 0))[:400, 160:560]
-        wp = wp1 + wp2
-
-        surround_map = np.clip(
-            (
-                surround_map.astype(np.float32)
-                + self_car_map.astype(np.float32)
-                + wp.astype(np.float32)
-            ),
-            0,
-            255,
-        ).astype(np.uint8)
-
-        map_t1, box_info = render(traffic_meta.reshape(20, 20, 7), pixels_per_meter=20, t=1)
-        map_t1 = map_t1[:400, 160:560]
-        map_t1 = np.stack([map_t1, map_t1, map_t1], 2)
-        map_t1 = np.clip(map_t1.astype(np.float32) + self_car_map.astype(np.float32), 0, 255).astype(np.uint8)
-        map_t1 = cv2.resize(map_t1, (200, 200))
-        map_t2, box_info = render(traffic_meta.reshape(20, 20, 7), pixels_per_meter=20, t=2)
-        map_t2 = map_t2[:400, 160:560]
-        map_t2 = np.stack([map_t2, map_t2, map_t2], 2)
-        map_t2 = np.clip(map_t2.astype(np.float32) + self_car_map.astype(np.float32), 0, 255).astype(np.uint8)
-        map_t2 = cv2.resize(map_t2, (200, 200))
-
+        
 
         if self.step % 2 != 0 and self.step > 4:
             control = self.prev_control
         else:
             self.prev_control = control
-            self.prev_surround_map = surround_map
 
-        tick_data["map"] = self.prev_surround_map
-        tick_data["map_t1"] = map_t1
-        tick_data["map_t2"] = map_t2
         tick_data["rgb_raw"] = tick_data["rgb"]
         tick_data["rgb_left_raw"] = tick_data["rgb_left"]
         tick_data["rgb_right_raw"] = tick_data["rgb_right"]
+
 
         tick_data["rgb"] = cv2.resize(tick_data["rgb"], (800, 600))
         tick_data["rgb_left"] = cv2.resize(tick_data["rgb_left"], (200, 150))
@@ -562,12 +480,9 @@ class DiffuseDriveAgent(autonomous_agent.AutonomousAgent):
             control.steer,
             control.brake,
         )
+        # TODO Jacopo: i think this is not necessary
         tick_data["meta_infos"] = meta_infos
-        tick_data["box_info"] = "car: %d, bike: %d, pedestrian: %d" % (
-            box_info["car"],
-            box_info["bike"],
-            box_info["pedestrian"],
-        )
+        
         tick_data["mes"] = "speed: %.2f" % velocity
         tick_data["time"] = "time: %.3f" % timestamp
         surface = self._hic.run_interface(tick_data)
